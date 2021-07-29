0385cac03a4fc4fefc0e08d79ca0b8cb9fb207d9
Kireeva <kireeva.en@defo.ru>
2021-07-29 12:43:07 +0300

    меню бургер на главной

--- js/script.js
+++ js/script.js
@@ -612,4 +612,13 @@ function lerp({ x, y }, { x: targetX, y: targetY }) {
   
 
 
-// ===============================================================
+// ==================   Меню - бургер   =========================
+$(document).ready(function() {
+	$('.header__burger').click(function(event) {
+		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+	$('.header__link').click(function(event) {
+		$('.header__burger,.header__menu').removeClass('active');
+	});
+});
